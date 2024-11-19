a8b978afe89536b4fc2f0e964a6df24087f9cf92
Danny <29934021+mtlaso@users.noreply.github.com>
2024-11-18 22:11:44 -0500

    nvim

--- .config/nvim/lua/danny/plugins/modicator.lua
+++ .config/nvim/lua/danny/plugins/modicator.lua
@@ -1,11 +1,10 @@
-return {}
--- return {
--- 	"mawkler/modicator.nvim",
--- 	init = function()
--- 		-- These are required for Modicator to work
--- 		vim.o.cursorline = true
--- 		vim.o.number = true
--- 		vim.o.termguicolors = true
--- 	end,
--- 	opts = {},
--- }
+return {
+	"mawkler/modicator.nvim",
+	init = function()
+		-- These are required for Modicator to work
+		vim.o.cursorline = true
+		vim.o.number = true
+		vim.o.termguicolors = true
+	end,
+	opts = {},
+}
